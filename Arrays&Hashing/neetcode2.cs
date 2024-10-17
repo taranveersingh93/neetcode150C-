2564f002edb69eabd98257ce71f944599377e2f3
Taranveer Singh <taranveersingh93@gmail.com>
2024-10-17 13:34:45 -0400

    Add solution - is anagram

--- Arrays&Hashing/neetcode2.cs
+++ Arrays&Hashing/neetcode2.cs
@@ -16,3 +16,47 @@
 // Constraints:
 
 // s and t consist of lowercase English letters.
+
+public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        Dictionary<char, int> sLog = new Dictionary<char, int>();
+        Dictionary<char, int> tLog = new Dictionary<char, int>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (sLog.ContainsKey(s[i]))
+            {
+                sLog[s[i]]++;
+            }
+            else
+            {
+                sLog[s[i]] = 1;
+            }
+        }
+        for (int j = 0; j < t.Length; j++)
+        {
+            if (tLog.ContainsKey(t[j]))
+            {
+                tLog[t[j]]++;
+            }
+            else
+            {
+                tLog[t[j]] = 1;
+            }
+        }
+
+        foreach (char key in sLog.Keys)
+        {
+            if (!tLog.ContainsKey(key))
+            {
+                return false;
+            }
+            if (tLog[key] != sLog[key])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
