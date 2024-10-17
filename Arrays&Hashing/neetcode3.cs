310b78f6caa9fad9c2b17982f3ee1e411af4c07f
Taranveer Singh <taranveersingh93@gmail.com>
2024-10-17 14:29:50 -0400

    Add solution - two integer sum

--- Arrays&Hashing/neetcode3.cs
+++ Arrays&Hashing/neetcode3.cs
@@ -28,3 +28,23 @@
 // 2 <= nums.length <= 1000
 // -10,000,000 <= nums[i] <= 10,000,000
 // -10,000,000 <= target <= 10,000,000
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+}
