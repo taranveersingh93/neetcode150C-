6fc1725d1e6ea3971021a3aebd40ab5a73277c48
Taranveer Singh <taranveersingh93@gmail.com>
2025-01-08 16:56:49 -0500

    Add solution

--- Arrays&Hashing/neetcode5.cs
+++ Arrays&Hashing/neetcode5.cs
@@ -20,3 +20,43 @@
 // 1 <= nums.length <= 10^4.
 // -1000 <= nums[i] <= 1000
 // 1 <= k <= number of distinct elements in nums.
+public class Solution
+{
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+        Dictionary<int, int> numStore = new Dictionary<int, int>();
+        foreach (int num in nums)
+        {
+            if (!numStore.ContainsKey(num))
+            {
+                numStore[num] = 0;
+            }
+            numStore[num]++;
+        }
+
+        List<List<int>> bucketList = new List<List<int>>();
+
+        foreach (int numKey in numStore.Keys)
+        {
+            int numFrequency = numStore[numKey];
+
+            while (bucketList.Count <= numFrequency)
+            {
+                bucketList.Add(new List<int>());
+            }
+
+            bucketList[numFrequency].Add(numKey);
+        }
+
+        List<int> outputList = new List<int>();
+
+        for (int i = bucketList.Count - 1; i >= 0 && outputList.Count < k; i--)
+        {
+            if (bucketList[i] != null && bucketList[i].Count > 0)
+            {
+                outputList.AddRange(bucketList[i]);
+            }
+        }
+        return outputList.ToArray();
+    }
+}
